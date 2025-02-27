d1daa63c3b39fd3780a05eaed412605affe46d7b
beetak <beetaktj1@gmail.com>
2025-02-27 09:00:02 +0200

    demo commit

--- src/components/products/Property/PropertyInsuranceViewModal.jsx
+++ src/components/products/Property/PropertyInsuranceViewModal.jsx
@@ -23,8 +23,9 @@ export default function PropertyInsuranceViewModal(props) {
                 <div className="block border border-gray-200 p-8 mt-3 rounded-sm">                    
                     <div className="flex flex-col items-center text-gray-600 space-y-3">
                         <h2 className="text-lg font-semibold text-gray-600"><span className='font-semibold text-xs'>Product Name: </span>{formatPolicyName(props.data.policy)}</h2>
-                        <p className=''><span className='font-semibold text-xs'>Policy Type: </span>{props.data.description}</p>
-                        <p className=''><span className='font-semibold text-xs'>Policy Type: </span>{props.data.rate}</p>
+                        <p className=''><span className='font-semibold text-xs'>Policy Description: </span>{props.data.description}</p>
+                        <p className=''><span className='font-semibold text-xs'>Insurance Company: </span>{props.data.insurerName}</p>
+                        <p className=''><span className='font-semibold text-xs'>Policy Rate: </span>{props.data.rate}</p>
                         <div className='w-full justify-center flex items-center'> <span className={` font-semibold uppercase text-xs tracking-wider px-3 text-white ${props.data.active?" bg-green-600": " bg-red-600 "} rounded-full py-1`}>{props.data.active?"Active":"Inactive"}</span></div>
                     </div>
                 </div>
